9e677c367b5a5d7da8fa05f57b154a5b2edd4878
yevdeh <evgeniydegtyaryov@gmail.com>
2023-01-19 16:58:51 +0300

    JS: Navigation and Risks modules

--- mobile/js/index.js
+++ mobile/js/index.js
@@ -1,83 +1,105 @@
 (function () {
-	var sectionsMain = [
-		'SectionHome',
-		'SectionQuiz1',
-		'SectionQuiz2',
-		'SectionQuiz3',
-		'SectionAnalyzing',
-		'SectionDownload',
-	]
-	var sectionsHardcore = [
-		'SectionHome',
-		'SectionQuiz1',
-		'SectionQuiz2',
-		'SectionQuiz3',
-		'SectionQuiz4',
-		'SectionQuiz5',
-		'SectionQuiz6',
-		'SectionQuiz7',
-		'SectionQuiz8',
-		'SectionQuiz9',
-		'SectionQuiz10',
-		'SectionQuiz11',
-		'SectionQuiz12',
-		'SectionQuiz13',
-		'SectionQuiz14',
-		'SectionQuiz15',
-		'SectionAnalyzing',
-		'SectionResults',
-	]
+	var Navigation = (function () {
+		function getSections() {
+			var sectionIdsMain = [
+				'SectionHome',
+				'SectionQuiz1',
+				'SectionQuiz2',
+				'SectionQuiz3',
+				'SectionAnalyzing',
+				'SectionDownload',
+			]
+			var sectionIdsHardcore = [
+				'SectionHome',
+				'SectionQuiz1',
+				'SectionQuiz2',
+				'SectionQuiz3',
+				'SectionQuiz4',
+				'SectionQuiz5',
+				'SectionQuiz6',
+				'SectionQuiz7',
+				'SectionQuiz8',
+				'SectionQuiz9',
+				'SectionQuiz10',
+				'SectionQuiz11',
+				'SectionQuiz12',
+				'SectionQuiz13',
+				'SectionQuiz14',
+				'SectionQuiz15',
+				'SectionAnalyzing',
+				'SectionResults',
+			]
+			var quizType = document.body.dataset.quizType
+			var sectionIds = quizType === 'main' ? sectionIdsMain : sectionIdsHardcore
+			return sectionIds.map(function (sectionId) {
+				return document.getElementById(sectionId)
+			})
+		}
+
+		var sections = getSections()
+		var activeSectionId = 0
+		var classSectionActive = 'Section--active'
+		var isNavigationBlocked = false
+
+
+		function goToPage(direction) {
+			if (isNavigationBlocked) return
+			isNavigationBlocked = true
+			setTimeout(() => {
+				sections[activeSectionId].classList.remove(classSectionActive)
+				sections[activeSectionId + direction].classList.add(classSectionActive)
+				activeSectionId += direction
+				isNavigationBlocked = false
+			}, 300);
+		}
+
+		function goNextPage() {
+			goToPage(1)
+		}
+
+		function goPrevPage() {
+			goToPage(-1)
+		}
+
+		return {
+			goNextPage: goNextPage,
+			goPrevPage: goPrevPage,
+		}
+	}())
+
+	var Risks = (function () {
+		function getRandomInteger(min, max) {
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}
+
+		function fill() {
+			var maxPoints = 58
+			var points = 0
+			document.querySelectorAll('.Option[data-is-selected="true"]').forEach(function ($option) {
+				points += parseInt($option.dataset.points, 10)
+			})
+			var min = 25
+			var max = 80
+			var dispersion = 10
+			var minMiddle = min + dispersion
+			var maxMiddle = max - dispersion
+			var percentPerPoint = (maxMiddle - minMiddle) / maxPoints
+			var percentsToExtract = percentPerPoint * points
+			var middle = maxMiddle - percentsToExtract
+			document.querySelectorAll('.Risk').forEach(function ($risk) {
+				percent = Math.round(middle - dispersion + getRandomInteger(0, dispersion * 2))
+				$risk.querySelector('.Risk-percent').innerText = percent
+				$risk.querySelector('.Risk-barFilled').style.width = percent + '%'
+			})
+		}
+
+		return {
+			fill: fill,
+		}
+	}())
+
 	var quizType = document.body.dataset.quizType
-	var sections = quizType === 'main' ? sectionsMain : sectionsHardcore
 	var lastSectionSelector = quizType === 'main' ? '#SectionQuiz3' : '#SectionQuiz15'
-	var $sections = sections.map(function (sectionId) { return document.getElementById(sectionId) })
-	var activeSectionId = 0
-	var classSectionActive = 'Section--active'
-	var isNavigationBlocked = false
-
-	function goToPage(direction) {
-		if (isNavigationBlocked) return
-		isNavigationBlocked = true
-		setTimeout(() => {
-			$sections[activeSectionId].classList.remove(classSectionActive)
-			$sections[activeSectionId + direction].classList.add(classSectionActive)
-			activeSectionId += direction
-			isNavigationBlocked = false
-		}, 300);
-	}
-
-	function goNextPage() {
-		goToPage(1)
-	}
-
-	function goPrevPage() {
-		goToPage(-1)
-	}
-
-	function getRandomInteger(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
-
-	function fillRisks() {
-		var maxPoints = 58
-		var points = 0
-		document.querySelectorAll('.Option[data-is-selected="true"]').forEach(function ($option) {
-			points += parseInt($option.dataset.points, 10)
-		})
-		var min = 25
-		var max = 80
-		var dispersion = 10
-		var minMiddle = min + dispersion
-		var maxMiddle = max - dispersion
-		var percentPerPoint = (maxMiddle - minMiddle) / maxPoints
-		var percentsToExtract = percentPerPoint * points
-		var middle = maxMiddle - percentsToExtract
-		document.querySelectorAll('.Risk').forEach(function ($risk) {
-			percent = Math.round(middle - dispersion + getRandomInteger(0, dispersion * 2))
-			$risk.querySelector('.Risk-percent').innerText = percent
-			$risk.querySelector('.Risk-barFilled').style.width = percent + '%'
-		})
-	}
 
 	// addEventListener for multiple elements
 	function on(nodes, callback, event) {
@@ -86,9 +108,9 @@
 		}
 	}
 
-	on(document.querySelectorAll('.Navigation-back'), goPrevPage)
+	on(document.querySelectorAll('.Navigation-back'), Navigation.goPrevPage)
 
-	on(document.querySelectorAll('#SectionHome .Button'), goNextPage)
+	on(document.querySelectorAll('#SectionHome .Button'), Navigation.goNextPage)
 
 	on(document.querySelectorAll('.Option'), function (e) {
 		var $optionSelected = e.target
@@ -97,7 +119,7 @@
 			$option.dataset.isSelected = false
 		});
 		$optionSelected.dataset.isSelected = true
-		goNextPage()
+		Navigation.goNextPage()
 	})
 
 	on(document.querySelectorAll(lastSectionSelector + ' .Option'), function () {
@@ -105,9 +127,9 @@
 			document.querySelector('.SectionAnalyzing-loader').play()
 			document.querySelector('.ListWithCheckboxes').classList.add('ListWithCheckboxes--animated')
 			setTimeout(function () {
-				goNextPage()
+				Navigation.goNextPage()
 				if (quizType === 'hardcore-with-risks') {
-					fillRisks()
+					Risks.fill()
 				}
 				if (quizType === 'hardcore-with-images') {
 					setTimeout(function () {
