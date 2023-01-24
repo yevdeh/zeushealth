00a0bb8501f3266eabb1c0986b6e5127a137ca35
yevdeh <evgeniydegtyaryov@gmail.com>
2023-01-24 14:28:40 +0300

    desktop: checkout page: popup

--- desktop/js/page-checkout.js
+++ desktop/js/page-checkout.js
@@ -13,6 +13,24 @@
 		})
 	}())
 
+	var Form = (function () {
+		var button = document.querySelector('.Form-button')
+		var inputs = document.querySelectorAll('.Form-input')
+
+		button.addEventListener('click', function () {
+			if (!document.querySelectorAll('.Form-input:invalid').length) {
+			}
+		})
+
+		on(inputs, function () {
+			if (this.checkValidity() && !document.querySelectorAll('.Form-input:invalid').length) {
+				button.classList.remove('Button--disabled')
+			} else {
+				button.classList.add('Button--disabled')
+			}
+		}, 'input')
+	}())
+
 	var PlanCards = (function () {
 		var cards = document.querySelectorAll('.choose-plan-card-template-module_planCard__1zLoM')
 		var classSelected = 'choose-plan-card-template-module_isSelected__bTfwV'
@@ -28,12 +46,19 @@
 
 	var Popup = (function () {
 		var buttons = document.querySelectorAll('[data-button="checkout-get-plan"]')
+		var closeAndShadow = document.querySelectorAll('.Popup-close, .Popup-shadow')
+		var popup = document.querySelector('.Popup')
+
+		function closePopup() {
+			popup.classList.add('Popup--hidden')
+		}
 
 		function openPopup() {
-
+			popup.classList.remove('Popup--hidden')
 		}
 
 		on(buttons, openPopup)
+		on(closeAndShadow, closePopup)
 	}())
 
 	var Timer = (function () {
