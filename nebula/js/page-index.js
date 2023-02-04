5856f933a7e40cfcbab92a322b36cec86edadee7
yevdeh <evgeniydegtyaryov@gmail.com>
2023-02-04 15:42:31 +0300

    nebula: index page

---
+++ nebula/js/page-index.js
@@ -0,0 +1,10 @@
+(function () {
+	var arrow = document.querySelector('.sc-gzrROc')
+
+	function animateArrow() {
+		arrow.classList.toggle('gmZxbv')
+		arrow.classList.toggle('hfJfWq')
+	}
+
+	setInterval(animateArrow, 1000);
+}())
